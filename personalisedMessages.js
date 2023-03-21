5137011192155f28e207b3a3ebb43caef4e36728
Valeria Serci <balerty84@gmail.com>
2023-03-21 15:02:36 +0000

    fix the function with a return

--- personalisedMessages.js
+++ personalisedMessages.js
@@ -2,8 +2,9 @@ const names = ['Anna', 'Laura', 'Josh', 'Min', 'Karla'];
 
 const generateMessages = (names) => {
   const message = names.map(name => {
-    console.log(`Hi ${name}! 50% off your best candies for you today!`);
+    return `Hi ${name}! 50% off your best candies for you today!`;
   });
+  return message
 };
 
-generateMessages(names);
+console.log(generateMessages(names));
